061446d7954e0dad7d3131c02e7c118a547ebf14
vitorgripa <vitor.gripa98@gmail.com>
2020-08-19 21:54:11 -0300

    Criado rotas para manipulação da tabela pelagem

--- src/controllers/PelagemController.js
+++ src/controllers/PelagemController.js
@@ -1,39 +1,81 @@
 const Pelagem = require('../models/Pelagem');
+const HTTPStatus = require('http-status');
 
 module.exports = {
     async index(req, res) {
-        const pelagens = await Pelagem.findAll();
-        return res.json(pelagens);
+        try {
+
+            const where = {};
+
+            if (req.query.nome) {
+                where.nome = req.query.nome;
+            }
+
+            const pelagens = await Pelagem.findAll({
+                where
+            });
+
+            return res.json(pelagens)
+        } catch(err) {
+            return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ messagem: "Erro ao listar as pelagens!" });
+        }
     },
 
     async store(req, res) {
-        const { nome } = req.body;
 
-        const pelagem = await Pelagem.create({ nome });
+        try {
+            const { nome } = req.body;
 
-        return res.json(pelagem);
+            console.log(nome)
+
+            if (!nome) {
+				return res.status(HTTPStatus.BAD_REQUEST).json({ erro: 'nome não informado!' });
+            }
+
+            await Pelagem.create({ nome });
+
+			return res.status(HTTPStatus.OK).json({ messagem: "Pelagem cadastrada com sucesso!" });
+        } catch(err) {
+			return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ messagem: "Erro ao cadastrar a pelagem!" });
+        }
     },
 
     async update(req, res) {
-        const { nome } = req.body;
 
-        await Pelagem.update({
-            nome
-        }, {
-            where: {
-                id: req.params.id
+        try {
+            const { nome } = req.body
+
+            if (!nome) {
+                return res.status(HTTPStatus.BAD_REQUEST).json({ erro: 'nome não informado!' });
             }
-        });
 
-        return res.json({message: "Registro alterado com sucesso!"})
+            await Pelagem.update({
+                nome
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            });
+
+			return res.status(HTTPStatus.OK).json({ messagem: "Pelagem alterada com sucesso!" });
+        } catch(err) {
+            return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({ messagem: "Erro ao alterar a pelagem!" });
+        }
     },
 
     async delete(req, res) {
-        await Pelagem.destroy({
-            where: {
-                id: req.params.id
-            }
-        });
-        return res.json({message: "Registro excluído com sucesso!"})
+
+        try {
+            await Pelagem.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+
+			return res.status(HTTPStatus.OK).json({ messagem: "Pelagem deletada com sucesso!" });
+        } catch(err) {
+            console.log(err)
+            return res.json({message: "Erro ao deletar a pelagem!"})
+        }
     }
 }
